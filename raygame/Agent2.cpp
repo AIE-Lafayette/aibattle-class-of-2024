ec0a55df40ced137ffed88aa4352c251061d5d2b
Galaxy <bryon.guidry1@gmail.com>
2023-03-06 11:01:29 -0600

    Agent2 implementation

--- raygame/Agent2.cpp
+++ raygame/Agent2.cpp
@@ -1,27 +1,45 @@
 #include "Agent2.h"
 #include "SpriteComponent.h"
-#include "GameManager.h"
+#include "SeekComponent.h"
+#include "EvadeComponent.h"
 #include "Goal.h"
+#include "GameManager.h"
+GameManager* gameInstance = GameManager::getInstance();
 
 Agent2::Agent2(float x, float y, const char* name, float maxForce, float maxSpeed, float health) : Character(x, y, name, maxForce, maxSpeed, health)
 {
-	addComponent(new SpriteComponent("Images/enemy.png"));
+	addComponent(new SpriteComponent("Images/player.png"));
 }
 
 void Agent2::onCollision(Actor* actor)
 {
 	Character::onCollision(actor);
+
+	if (actor == gameInstance->getAgent2())
+		gameInstance->getAgent2()->takeDamage();
 }
 
 void Agent2::start()
 {
-	Character::start(); 
+	Character::start();
+
+	addComponent(new SeekComponent(gameInstance->getBall(), 1000, this));
+	addComponent(new EvadeComponent(gameInstance->getAgent2(), 500, this));
 	//add steering behaviours here
 }
 
 void Agent2::update(float deltaTime)
 {
 	Character::update(deltaTime);
+
+	if (getHasBall())
+	{
+		getComponent<SeekComponent>()->setTarget(gameInstance->getRightGoal());
+	}
+	else
+	{
+		getComponent<SeekComponent>()->setTarget(gameInstance->getBall());
+	}
 
 }
 
